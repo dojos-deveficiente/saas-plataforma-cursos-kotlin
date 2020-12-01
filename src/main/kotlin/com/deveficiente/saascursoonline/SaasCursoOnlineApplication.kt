a8ca6162d972e896a2fe4a351ea2986adb616e96
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-12-01 20:16:29 -0300

    iniciando

---
+++ src/main/kotlin/com/deveficiente/saascursoonline/SaasCursoOnlineApplication.kt
@@ -0,0 +1,11 @@
+package com.deveficiente.saascursoonline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SaasCursoOnlineApplication
+
+fun main(args: Array<String>) {
+	runApplication<SaasCursoOnlineApplication>(*args)
+}
---
+++ src/test/kotlin/com/deveficiente/saascursoonline/SaasCursoOnlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deveficiente.saascursoonline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SaasCursoOnlineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
